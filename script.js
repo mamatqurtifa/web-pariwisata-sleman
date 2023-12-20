90d2c28df5898435fb683264bf9ad9ea10f0912f
mamatqurtifa <146681551+mamatqurtifa@users.noreply.github.com>
2023-12-21 02:49:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Perbuatan Variabel
+const nav = document.querySelector(".nav"),
+  searchIcon = document.querySelector("#searchIcon"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+// Code agar search bar muncul ketika tombol search diklik, namun fungsi search belum dapat difungsikan
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("uil-search", "uil-times");
+  }
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+
+// code agar menu navigasi dapat muncul dari samping kiri dilayar hp ketika tombol humberger diklik
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
